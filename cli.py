bf06f019f7ef0349126041bb77cb1ba32849f083
Mateo-Guillen <mmateoguillen@gmail.com>
2023-06-23 15:44:05 -0500

    Add Json manager in addition, I add the read_Json func that reads each of the persons in the data.json

--- cli.py
+++ cli.py
@@ -1,8 +1,17 @@
 import  click
+import json_manager
 @click.group() #Que hace esto??
 
 def cli():
     pass
+
+@cli.command()
+def users():
+    users = json_manager.read_json()
+    for user in users:
+        print(f"{user['id']} - {user['name']} - {user['lastname']}")
+        
+
 if __name__ == "__main__":
     cli()
 
